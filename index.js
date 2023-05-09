20ca37f2e135f9065326b3e472979d5e7e9f1e37
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2023-05-09 19:44:28 +0200

    Scrape website for data

--- index.js
+++ index.js
@@ -6,4 +6,40 @@ const express = require('express')
 
 const app = express()
 
+const url = 'https://www.treadmtb.co.za'
+
+//app.METHOD(PATH, HANDLER)
+app.get() //get data
+
+app.post() //add data
+
+app.put() //edit data
+
+app.delete() //delete data
+
+
+
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+
+        $('.title',html).each(function(){
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+
+        })
+        console.log(articles)
+
+    }).catch(err => console.log(err))
+
+
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
